2a69f26c6a64917f139120aca285020c1080b060
Abhinandan <abhinandannarang3@gmail.com>
2023-03-22 00:12:30 +0530

    Upto radio button.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -7,11 +17,129 @@ public class App {
     }
 }
 
+
 class ABC extends JFrame{
-    public ABC(){
-        JLabel l1 = new JLabel("Hello");
+    JTextField jTextField = null;
+    JRadioButton jr1 = null, jr2 = null;
+    JLabel jl = null;    
+    JButton jButton = null;
+
+     public ABC(){
+
+        jTextField = new JTextField(20);
+        jr1 = new JRadioButton("Male");
+        jr2 = new JRadioButton("Female");
+        jButton = new JButton("Enter");
+        jl = new JLabel();
+        ButtonGroup buttonGroup = new ButtonGroup();
+        buttonGroup.add(jr1);
+        buttonGroup.add(jr2);
+        add(jTextField);
+        add(jButton);
+        add(jr1);
+        add(jr2);
+        add(jl);
+
+        jButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO Auto-generated method stub
+                String string = jTextField.getText();
+                if(jr1.isSelected()){
+                    string = "Mr."+string;
+                }
+                else if(jr2.isSelected()){
+                    string = "Mrs."+string;
+
+                }
+                jl.setText(string);
+            }
+            
+        });
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setSize(400, 400);
-        
+        setLayout(new FlowLayout());
     }
 }
+
+
+// class ABC extends JFrame /*implements ActionListener*/{
+//     JTextField t2= null;
+//     JTextField t1= null;
+//     JLabel j1= null;
+
+//     JRadioButton r1, r2;
+
+//     public ABC(){
+//         // JLabel l1 = new JLabel("Hello");
+//         // add(l1);
+        
+//         t1 = new JTextField(20);
+//         t2 = new JTextField(20);
+
+//         Button b1 = new Button("Calculate");
+//         j1 =new JLabel("Result");
+//         add(t1);add(t2);add(b1);add(j1);
+//         // int a = t1.
+
+//         ///b1.addActionListener(this);
+//         // b1.addActionListener(new ActionListener() {
+            
+//         //     @Override
+//         //     public void actionPerformed(ActionEvent e) {
+//         //         // TODO Auto-generated method stub
+//         //         int num1 = Integer.parseInt(t1.getText()); 
+//         //         int num2 = Integer.parseInt(t2.getText());
+//         //         int num3 = num1+num2;
+//         //         j1.setText(num3+ "");
+//         //     }
+//         // });
+
+//         r1 = new JRadioButton("Male");
+//         r2 = new JRadioButton("Female");
+//         add(r1);add(r2);
+//         ButtonGroup buttonGroup = new ButtonGroup();
+//         buttonGroup.add(r1);
+//         buttonGroup.add(r2);
+
+//         b1.addActionListener(new ActionListener() {
+
+//             @Override
+//             public void actionPerformed(ActionEvent e) {
+//                 // TODO Auto-generated method stub
+//                 JLabel jj=null;
+//                 String string = "";
+//                 if(r1.isSelected()){
+//                     string +="Mr."; 
+//                 }
+//                 else if (r2.isSelected()){
+//                     string +="Ms."; 
+//                 }
+//                 jj.setText(string);
+//                 add(jj);
+//             }
+            
+//         });
+        
+
+//         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//         setVisible(true);
+//         setSize(400, 400);
+//         setLayout(new FlowLayout());
+        
+//     }
+
+    // @Override
+    // public void actionPerformed(ActionEvent e) {
+    //     // TODO Auto-generated method stub
+    //     int num1 = Integer.parseInt(t1.getText()); 
+    //     int num2 = Integer.parseInt(t2.getText());
+           //if(ae.getSource()==b1)     to check which button was pressed
+    //     int num3 = num1+num2;
+    //     j1.setText(num3+ "");
+    //     //throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    // }
+//}
