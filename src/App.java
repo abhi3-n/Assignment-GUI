87a9dbddb64b4fc0e51e561dacbcad3a41d2bb36
Abhinandan <abhinandannarang3@gmail.com>
2023-04-09 01:04:08 +0530

    Multi Panel basics.

--- src/App.java
+++ src/App.java
@@ -1,8 +1,14 @@
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class App {
     
@@ -17,13 +23,28 @@ class InitialPage extends JFrame{
            RollNo = null,
            ProjName = null;
     JButton Start = null;
-
+    JButton backButton = null;
+    JPanel contPanel = null;
+    JPanel InitialPanel = null;
+    JPanel RegisterPagePanel = null;
+    CardLayout cardLayout = null;
     InitialPage(){
-        int labelwidth = 200, labelheight = 50;
+        setTitle("Assignment");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setSize(600, 600);
-        setLayout(null);
+        //setLayout(new CardLayout());
+        cardLayout = new CardLayout();
+
+        int labelwidth = 200,
+            labelheight = 50;
+
+        contPanel = new JPanel();
+        contPanel.setLayout(cardLayout);
+        InitialPanel = new JPanel();
+        InitialPanel.setBackground(Color.cyan);
+        RegisterPagePanel = new JPanel();
+        RegisterPagePanel.setBackground(Color.LIGHT_GRAY);
 
         name = new JLabel("Abhinandan Narang");
         name.setBounds(180,130,labelwidth,labelheight);
@@ -41,6 +62,34 @@ class InitialPage extends JFrame{
         Start.setBounds(230,300,labelwidth-100,labelheight-25);
         Start.setFont(new Font("Serif", Font.PLAIN,24));
 
-        add(name);add(RollNo);add(ProjName);add(Start);
+        backButton = new JButton("Go back");
+        backButton.setBounds(230,300,labelwidth-100,labelheight-25);
+        backButton.setFont(new Font("Serif", Font.PLAIN,24));
+
+        InitialPanel.add(name);
+        InitialPanel.add(RollNo);
+        InitialPanel.add(ProjName);
+        InitialPanel.add(Start);
+        RegisterPagePanel.add(backButton);
+        
+        contPanel.add(InitialPanel,"1");
+        contPanel.add(RegisterPagePanel,"2");
+        add(contPanel);
+        Start.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cardLayout.show(contPanel,"2");
+            }
+            
+        });
+        backButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cardLayout.show(contPanel,"1");
+            }
+            
+        });
     }
 }
