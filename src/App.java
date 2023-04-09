4b9484317430301f21d8dc67e3b022b06400f7fc
Abhinandan <abhinandannarang3@gmail.com>
2023-04-09 14:45:43 +0530

    First draft for multi panels using card layout.

--- src/App.java
+++ src/App.java
@@ -32,7 +32,9 @@ class InitialPage extends JFrame{
         setTitle("Assignment");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
-        setSize(600, 600);
+        setLayout(null);
+        setBounds(100,100,600,600);
+        //setSize(600, 600);
         //setLayout(new CardLayout());
         cardLayout = new CardLayout();
 
@@ -41,36 +43,43 @@ class InitialPage extends JFrame{
 
         contPanel = new JPanel();
         contPanel.setLayout(cardLayout);
+        contPanel.setBounds(0,0,600,600);
+
         InitialPanel = new JPanel();
+        InitialPanel.setBounds(0,0,600,600);
+        InitialPanel.setLayout(null);
         InitialPanel.setBackground(Color.cyan);
+
         RegisterPagePanel = new JPanel();
-        RegisterPagePanel.setBackground(Color.LIGHT_GRAY);
+        RegisterPagePanel.setBounds(0,0,600,600);
+        RegisterPagePanel.setLayout(null);
+        RegisterPagePanel.setBackground(Color.CYAN);
 
         name = new JLabel("Abhinandan Narang");
         name.setBounds(180,130,labelwidth,labelheight);
         name.setFont(new Font("Serif",Font.PLAIN,24));
+        InitialPanel.add(name);
 
         RollNo = new JLabel("2022mt93301");
         RollNo.setBounds(200,190,labelwidth,labelheight);
         RollNo.setFont(new Font("Serif",Font.PLAIN,24));
+        InitialPanel.add(RollNo);
 
         ProjName = new JLabel("Real estate Renting and Selling.");
         ProjName.setBounds(150,250,labelwidth+150,labelheight);
         ProjName.setFont(new Font("Serif",Font.PLAIN,24));
+        InitialPanel.add(ProjName);
 
         Start = new JButton("Start");
         Start.setBounds(230,300,labelwidth-100,labelheight-25);
         Start.setFont(new Font("Serif", Font.PLAIN,24));
+        InitialPanel.add(Start);
 
         backButton = new JButton("Go back");
         backButton.setBounds(230,300,labelwidth-100,labelheight-25);
         backButton.setFont(new Font("Serif", Font.PLAIN,24));
-
-        InitialPanel.add(name);
-        InitialPanel.add(RollNo);
-        InitialPanel.add(ProjName);
-        InitialPanel.add(Start);
         RegisterPagePanel.add(backButton);
+
         
         contPanel.add(InitialPanel,"1");
         contPanel.add(RegisterPagePanel,"2");
